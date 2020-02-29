9853384c63867db0c00cd19d8c056c8546efcb25
549032340 <549032340@qq.com>
2020-02-29 13:39:29 +0800

    职位收藏页面标题修改，你修改了什么地方的什么功能需要描述清楚，多人合作时，方便出错后的查找错误

--- src/views/collection/collection.vue
+++ src/views/collection/collection.vue
@@ -5,7 +5,7 @@
         <Layout :style="{width: '770px',minWidth: '700px'}">
           <Header>
         <div class="header">
-          <span>我的收藏</span>
+          <span>我的收藏--当多人同时操作同一代码时会造成冲突，需要修改冲突，只是本地代码冲突，修改完之后无需立即提交到master上，可以等自己的代码改完再提交</span>
         </div>
       </Header>
         </Layout>
