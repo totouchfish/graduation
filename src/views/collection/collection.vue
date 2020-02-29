647f874a1e9e9a4fc0df6863ee00f1866c48f7b5
Kongjuan <kongjuan_k@163.com>
2020-02-29 12:44:09 +0800

    collection收藏

--- src/views/collection/collection.vue
+++ src/views/collection/collection.vue
@@ -8,24 +8,6 @@
           <span>我的收藏</span>
         </div>
       </Header>
-          <!-- <Content class="content">
-            <div class="recruit" v-for="(item,index) in resumeData" :key="index">
-              <a class="recruit_left" href="#" style="color:#333">
-                <p class="recruit_right_postName">{{item.pName}}</p>
-                <p class="recruit_right_workInfo" :title="item.postRequirement" v-html="item.postRequirement2">{{item.postRequirement2}}</p>
-                <p style="font-size:14px;margin-top:11px;">{{item.publicTime}}</p>
-              </a>
-              <div class="recruit_right">
-                <a href="#" :title="item.companyName" style="color:#333">
-                  <p class="recruit_right_title">{{item.companyName}}</p>
-                  <p style="margin-top:8px;">{{item.trade}}</p>
-                  <p style="margin-top:2px;">
-                    <span class="recruit_right_label" v-for="(item,index2) in item.companyWelfare.slice(0,2)" :key="index2">{{item}}</span> ...
-                  </p>
-                </a>
-              </div>
-            </div>
-          </Content> -->
         </Layout>
       </Layout>
     </div>
